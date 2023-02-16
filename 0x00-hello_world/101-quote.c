de47d234713b4361a0da98921749fd772598081e
satoyume13 <90615453+satoyume13@users.noreply.github.com>
2023-02-17 00:14:29 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - prints out last quote
- *
- * Return: Always 0
- */
+* main - Prints the size of vcompiled and run on.
+*
+* Return: Always 1
+**/
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
-	putchar('\n');
-	return (0);
+const char
+*str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, str, 59);
+return (1);
 }
